cb98aa871358200779ba31ff1b4d7d79a937aa6d
MathyLima <mathy.l.freitas@gmail.com>
2023-09-28 18:03:33 -0300

    essa misera ai

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2')
+const path = require('path');
 const express = require('express');
 
 
@@ -55,3 +56,16 @@ app.get("/api", (req, res) => {
 app.listen(PORT,()=>{
     console.log(`Server listening on ${PORT}`)
 })
+
+// Fazer com que o Node sirva os arquivos do app em React criado
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Lidar com as solicitações GET feitas à rota /api
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// Todas as outras solicitações GET não tratadas retornarão nosso app em React
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
