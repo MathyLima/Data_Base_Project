ff39cb6604b10b7289a7c7b9901974df2a76c216
MathyLima <mathy.l.freitas@gmail.com>
2023-09-28 17:29:19 -0300

    conexão ao bd

--- server/index.js
+++ server/index.js
@@ -1,22 +1,41 @@
 const mysql = require('mysql2')
 const express = require('express');
 
-class connection{
-    constructor(host,user,password,database){
-        this.host = host
-        this.user = user
-        this.password = password
-        this.database = database
-        this.connected = connection.connect
+
+class Connection {
+    constructor(host, user, password, database) {
+        this.host = host;
+        this.user = user;
+        this.password = password;
+        this.database = database;
+        this.connected = false; // Initialize connected property
+
+        // Create the MySQL connection within the constructor
+        this.connection = mysql.createConnection({
+            host: this.host,
+            user: this.user,
+            password: this.password,
+            database: this.database
+        });
+
+        // Attempt to connect to the database
+        this.connect();
     }
-connection = mysql.createConnection({
-    host : this.host,
-    user: this.user,
-    password: this.password,
-    database: this.database
-});
-    
+
+    connect() {
+        // Establish a connection to the database
+        this.connection.connect((err) => {
+            if (err) {
+                console.error('Error connecting to the database:', err);
+                this.connected = false;
+            } else {
+                console.log('Connected to the database');
+                this.connected = true;
+            }
+        });
+    }
 }
+
 
 // connection.connect((error)=>{
 //     if(error){
